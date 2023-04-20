657c73507897dee8f6e025b73b7965c67eff69ce
hideonnet <showedMaker@163.com>
2023-04-20 20:42:18 +0800

    commit_master_1.0

---
+++ com.xxxx.Test/src/main/java/com/xxxx/Test/Test.java
@@ -0,0 +1,18 @@
+package com.xxxx.Test;
+
+/**
+ * @ClassName Test
+ * @Description
+ * @Author yzchao
+ * @Date 2023/4/20 19:47
+ * @Version V1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        //绿色代表添加暂存区 未提交到本地库
+        System.out.println("git_test_commit_1.0");
+    }
+
+
+}
