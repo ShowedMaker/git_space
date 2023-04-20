c9e1109018cd5b78064596fa520c216e4a7c80f5
hideonnet <showedMaker@163.com>
2023-04-20 20:44:42 +0800

    commit_master_2.0

--- com.xxxx.Test/src/main/java/com/xxxx/Test/Test.java
+++ com.xxxx.Test/src/main/java/com/xxxx/Test/Test.java
@@ -11,7 +11,9 @@ public class Test {
 
     public static void main(String[] args) {
         //绿色代表添加暂存区 未提交到本地库
+        //蓝色代表未追踪过 但是修改过
         System.out.println("git_test_commit_1.0");
+        System.out.println("git_test_commit_2.0");
     }
 
 
